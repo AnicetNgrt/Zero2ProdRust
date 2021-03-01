b65e06c2549d9d762b54f175e15231ac71b8168b
Anicet Ngrt <anicet.nougaret@zohomail.eu>
2021-03-01 11:26:56 +0100

    fixed main

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,12 @@ use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0")
+    let listener = TcpListener::bind("127.0.0.1:8000")
         .expect("Failed to bind random port");
+    
+    // midly useless
+    let port = listener.local_addr().unwrap().port();
+    println!("http://127.0.0.1:{}", port);
+
     run(listener)?.await
 }
